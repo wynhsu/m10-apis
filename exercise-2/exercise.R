9750a58fed6c6dd814279912e3ebe4a5b440787c
wynhsu <wynhsu@uw.edu>
2016-10-26 19:09:34 -0700

    fin

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -6,15 +6,21 @@
 library(jsonlite)
 setwd('~/Documents/info-201/m10-apis/exercise-2/')
 
+data <- fromJSON('https://api.spotify.com/v1/search?q=nelly&type=artist')
 
 # Write a function that allows you to specify an artist, and returns the top 10 tracks of that artist
-
-
+TopTenTracks <- function(artist) {
+  base <- 'https://api.spotify.com/v1/'
+  artist.query <- paste0(base, 'search?', 'q=', artist, '&type=artist')
+  artist.info <- fromJSON(artist.query)
+  artist.id <- artist.info$artists$items$id[1]
+  album.query <- paste0(base, 'artists/', artist.id, '/top-tracks?country=US')
+  top.tracks <- fromJSON(album.query)
+  return(top.tracks$tracks)
+}
 
 # What are the top 10 tracks by Nelly?
-
-
-
+top.nelly <- TopTenTracks('Nelly')
 
 ### Bonus ### 
 # Write a function that allows you to specify a search type (artist, album, etc.), and a string, 
